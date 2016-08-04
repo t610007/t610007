998a743f1425fc8ce08b3d305915b7036d8ca318
t610007 <j30390@yahoo.com.tw>
2016-08-04 14:42:56 +0800

    Update kag.py

--- kag.py
+++ kag.py
@@ -2,6 +2,6 @@ import csv
 with open('tt.csv', newline='') as csvfile:
   spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
   for row in spamreader:
-    print row
+    print (row)
 
 name = input('請輸入你的名稱：')
