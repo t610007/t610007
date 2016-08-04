22c6183cd7c932780113bf6f7a476efe863daef5
t610007 <j30390@yahoo.com.tw>
2016-08-04 14:17:23 +0800

    Update kag.py

--- kag.py
+++ kag.py
@@ -1,3 +1,8 @@
+import csv  
+with open('test.csv','rb') as myFile:  
+    lines=csv.reader(myFile)  
+    for line in lines:  
+        print line
 print ('hello world')
 name = input('請輸入你的名稱：')
 print('歡迎 ', name)
