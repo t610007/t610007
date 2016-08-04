9738c779f566c47a43cc1a845ee9a9a1b964b01c
t610007 <j30390@yahoo.com.tw>
2016-08-04 14:27:23 +0800

    Update kag.py

--- kag.py
+++ kag.py
@@ -3,6 +3,5 @@ with open('tt.csv','rb') as myFile:
     lines=csv.reader(myFile)  
     for line in lines:  
         print line
-print ('hello world')
+
 name = input('請輸入你的名稱：')
-print('歡迎 ', name)
