876c18a9bb6627e53b0b41d0431a38fc7a47d0ab
t610007 <j30390@yahoo.com.tw>
2016-08-18 16:05:49 +0800

    Update kag.py

--- kag.py
+++ kag.py
@@ -1,7 +1,10 @@
-import csv
-with open('tt.csv', newline='') as csvfile:
-  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-  for row in spamreader:
+from sklearn.ensemble import RandomForestClassifier
+from numpy import genfromtxt, savetxt
+ dataset = genfromtxt(open('tt.csv', 'r'), delimiter = ',', dtype = 'f8')[1:]
+ target = [x[0] for x in dataset] # 第一列为label
+ train = [x[1:] for x in dataset]
+
+  for row in dataset:
     print (row)
 
 name = input('請輸入你的名稱：')
