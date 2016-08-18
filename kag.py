598a4c8db6bd24734a0400de331af888cd99ba8f
t610007 <j30390@yahoo.com.tw>
2016-08-18 16:11:32 +0800

    Update kag.py

--- kag.py
+++ kag.py
@@ -1,8 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from numpy import genfromtxt, savetxt
- dataset = genfromtxt(open('tt.csv', 'r'), delimiter = ',', dtype = 'f8')[1:]
- target = [x[0] for x in dataset] # 第一列为label
- train = [x[1:] for x in dataset]
+ dataset = genfromtxt('tt.csv', delimiter = ',', dtype = 'none')
+
 
   for row in dataset:
     print (row)
